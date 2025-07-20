c8f7e1146e092573465e4ae113d0a7c370989e62
Rob Marvin <fenalfan@gmail.com>
2025-07-20 03:49:51 +0300

    chore(config): add vitest config

---
+++ vitest.config.js
@@ -0,0 +1,31 @@
+import { mergeConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './setup.js',
+      coverage: {
+        checkCoverage: true,
+        include: ['src/**/*.{js,jsx,ts,tsx}'],
+        exclude: [
+          'src/**/*.test.{js,jsx,ts,tsx}',
+          'src/**/*.spec.{js,jsx,ts,tsx}',
+          'src/index.{js,jsx,ts,tsx}',
+          'src/setupTests.{js,ts}',
+          'src/**/*.d.ts',
+        ],
+        thresholds: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+    },
+  })
+);
