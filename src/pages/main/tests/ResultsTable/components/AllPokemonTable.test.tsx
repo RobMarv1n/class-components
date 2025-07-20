aadc8e568886a16287d28711fce29e5779cf80e7
Rob Marvin <fenalfan@gmail.com>
2025-07-20 23:23:38 +0300

    chore: update imports for mocks

--- src/pages/main/tests/ResultsTable/components/AllPokemonTable.test.tsx
+++ src/pages/main/tests/ResultsTable/components/AllPokemonTable.test.tsx
@@ -1,23 +1,14 @@
 import { describe, test, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import AllPokemonTable from '../../../components/ResultsTable/components/AllPokemonTable';
-
-const mockData = {
-  count: 2,
-  results: [
-    { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
-    { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
-  ],
-};
-
-const mockDataWithoutResults = {
-  count: 0,
-  results: [],
-};
+import {
+  allPokemonMock,
+  allPokemonWithoutResultsMock,
+} from '../../../../../shared/testsMocks/AllPokemonMocks';
 
 describe('AllPokemonTable', () => {
   test('Should render a table with Pokemon names and URLs', () => {
-    render(<AllPokemonTable data={mockData} />);
+    render(<AllPokemonTable data={allPokemonMock} />);
 
     expect(screen.getByText('Name')).toBeInTheDocument();
     expect(screen.getByText('URL')).toBeInTheDocument();
@@ -32,13 +23,13 @@ describe('AllPokemonTable', () => {
   });
 
   test('Should render correct number of rows', () => {
-    render(<AllPokemonTable data={mockData} />);
+    render(<AllPokemonTable data={allPokemonMock} />);
     const rows = screen.getAllByRole('row');
     expect(rows.length).toBe(3);
   });
 
   test('Should display a message stating that nothing was found.', () => {
-    render(<AllPokemonTable data={mockDataWithoutResults} />);
+    render(<AllPokemonTable data={allPokemonWithoutResultsMock} />);
     expect(screen.getByText('Nothing was found')).toBeInTheDocument();
   });
 });
