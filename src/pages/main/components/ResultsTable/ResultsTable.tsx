753104ff09fdbf3761a774e84b08f89eaee8ff49
Rob Marvin <fenalfan@gmail.com>
2025-07-24 13:46:57 +0300

    refactor: convert a ResultTable from a class component to a functional

--- src/pages/main/components/ResultsTable/ResultsTable.tsx
+++ src/pages/main/components/ResultsTable/ResultsTable.tsx
@@ -1,36 +1,31 @@
-import { Component } from 'react';
 import type { SinglePokemonData } from '../../../../shared/api/types/SinglePokemonTypes';
 import DataUploadError from '../../../../shared/ui/DataUploadError';
 import SinglePokemonTable from './components/SinglePokemonTable';
-import styles from './ResultsTable.module.css';
 import type { AllPokemonData } from '../../../../shared/api/types/AllPokemonTypes';
 import AllPokemonTable from './components/AllPokemonTable';
+import styles from './ResultsTable.module.css';
 
-class ResultsTable extends Component<ResultsTableProps> {
-  render() {
-    const { data, error } = this.props;
+function ResultsTable({ data, error }: ResultsTableProps) {
+  let content;
 
-    let content;
-
-    if (error) {
-      content = <DataUploadError message={error} />;
-    } else if (!data) {
-      content = <p>Nothing was found</p>;
-    } else if ('results' in data) {
-      content = <AllPokemonTable data={data} />;
-    } else if ('sprites' in data && 'height' in data) {
-      content = <SinglePokemonTable data={data} />;
-    } else {
-      content = <p>Unsupported data format</p>;
-    }
-
-    return <section className={styles.resultsTable}>{content}</section>;
+  if (error) {
+    content = <DataUploadError message={error} />;
+  } else if (!data) {
+    content = <p>Nothing was found</p>;
+  } else if ('results' in data) {
+    content = <AllPokemonTable data={data} />;
+  } else if ('sprites' in data && 'height' in data) {
+    content = <SinglePokemonTable data={data} />;
+  } else {
+    content = <p>Unsupported data format</p>;
   }
+
+  return <section className={styles.resultsTable}>{content}</section>;
 }
 
-export interface ResultsTableProps {
+export type ResultsTableProps = {
   data: AllPokemonData | SinglePokemonData | null;
   error: string | null;
-}
+};
 
 export default ResultsTable;
