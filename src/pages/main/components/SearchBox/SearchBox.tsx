19d6212eb6d0dbf40767f3c49a6f9852641339e2
Rob Marvin <fenalfan@gmail.com>
2025-07-24 14:08:13 +0300

    refactor: convert a SearchBox from a class component to a functional

--- src/pages/main/components/SearchBox/SearchBox.tsx
+++ src/pages/main/components/SearchBox/SearchBox.tsx
@@ -1,53 +1,42 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { START_SEARCH_ENDPOINT } from '../../../../shared/api/endpoints';
 import styles from './SearchBox.module.css';
 import Button from '../../../../shared/ui/Button/Button';
 import Input from '../../../../shared/ui/Input/Input';
 
-class SearchBox extends Component<SearchBoxProps, SearchBoxState> {
-  constructor(props: SearchBoxProps) {
-    super(props);
-    this.state = {
-      query: props.initialQuery ?? '',
-    };
-  }
+function SearchBox(props: SearchBoxProps) {
+  const { onSearch, placeholder, initialQuery } = props;
+  const [query, setQuery] = useState(initialQuery ?? '');
 
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ query: event.target.value });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
   };
 
-  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    this.props.onSearch(this.state.query.trim());
+    onSearch(query.trim());
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className={styles.searchBoxContainer}>
-          <p>{START_SEARCH_ENDPOINT}</p>
-          <Input
-            onChange={this.handleChange}
-            value={this.state.query}
-            placeholder={
-              this.props.placeholder || 'Enter Pokémon full name or id'
-            }
-            className={styles.searchInput}
-          />
-          <Button type="submit">Search</Button>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className={styles.searchBoxContainer}>
+        <p>{START_SEARCH_ENDPOINT}</p>
+        <Input
+          onChange={handleChange}
+          value={query}
+          placeholder={placeholder || 'Enter Pokémon full name or id'}
+          className={styles.searchInput}
+        />
+        <Button type="submit">Search</Button>
+      </div>
+    </form>
+  );
 }
 
-interface SearchBoxProps {
+type SearchBoxProps = {
   onSearch: (query: string) => void;
   placeholder?: string;
   initialQuery?: string;
-}
-interface SearchBoxState {
-  query: string;
-}
+};
 
 export default SearchBox;
