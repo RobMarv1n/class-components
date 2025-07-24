e200888b5e280fdc33e9cfcfa4791352ee14f632
Rob Marvin <fenalfan@gmail.com>
2025-07-24 18:35:58 +0300

    refactor: convert a Button from a class component to a functional

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,21 +1,19 @@
-import { type ComponentPropsWithoutRef, Component } from 'react';
+import { type ComponentPropsWithoutRef } from 'react';
 import styles from './Button.module.css';
 
-class Button extends Component<ButtonProperties> {
-  render() {
-    const { className, type, disabled, onClick, children } = this.props;
+function Button(props: ButtonProperties) {
+  const { className, type, disabled, onClick, children } = props;
 
-    return (
-      <button
-        type={type || 'button'}
-        className={`${styles.button} ${className || ''}`}
-        disabled={disabled}
-        onClick={onClick}
-      >
-        {children}
-      </button>
-    );
-  }
+  return (
+    <button
+      type={type || 'button'}
+      className={`${styles.button} ${className || ''}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
 
 type ButtonProperties = ComponentPropsWithoutRef<'button'>;
