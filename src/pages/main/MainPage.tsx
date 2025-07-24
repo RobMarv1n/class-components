c34ad5829075d4d967323177903adce7add195b7
Rob Marvin <fenalfan@gmail.com>
2025-07-24 18:26:36 +0300

    refactor: add useLocalStorage hook in MainPage

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,47 +1,47 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import type { SinglePokemonData } from '../../shared/api/types/SinglePokemonTypes';
 import Spinner from '../../shared/ui/Spinner/Spinner';
 import ResultsTable from './components/ResultsTable/ResultsTable';
 import SearchBox from './components/SearchBox/SearchBox';
 import { searchPokemon } from '../../shared/api/service/pokemon.service';
 import type { AllPokemonData } from '../../shared/api/types/AllPokemonTypes';
+import useLocalStorage from '../../shared/hooks/useLocalStorage';
 
 const LAST_POKEMON_SEARCH = '[LAST_POKEMON_SEARCH]';
 
 function MainPage() {
-  const [result, setResult] = useState<
-    AllPokemonData | SinglePokemonData | null
-  >(null);
+  const [result, setResult] = useState<ResultsType>(null);
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [isCrashing, setIsCrashing] = useState(false);
-  const [lastQuery] = useState(
-    () => localStorage.getItem(LAST_POKEMON_SEARCH) || ''
+  const [lastQuery, setLastQuery] = useLocalStorage(LAST_POKEMON_SEARCH, '');
+
+  const handleSearch = useCallback(
+    async (query: string) => {
+      setLastQuery(query);
+      setError(null);
+      setIsLoading(true);
+
+      try {
+        const data = await searchPokemon(query);
+        setResult(data);
+      } catch (error_: unknown) {
+        const message =
+          error_ instanceof Error ? error_.message : 'Unknown error';
+        setResult(null);
+        setError(message);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [setLastQuery]
   );
 
   useEffect(() => {
     if (lastQuery) {
       handleSearch(lastQuery);
     }
-  }, [lastQuery]);
-
-  const handleSearch = async (query: string) => {
-    localStorage.setItem(LAST_POKEMON_SEARCH, query);
-    setError(null);
-    setIsLoading(true);
-
-    try {
-      const data = await searchPokemon(query);
-      setResult(data);
-    } catch (error_: unknown) {
-      const message =
-        error_ instanceof Error ? error_.message : 'Unknown error';
-      setResult(null);
-      setError(message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  }, [lastQuery, handleSearch]);
 
   const triggerCrash = () => {
     setIsCrashing(true);
@@ -60,5 +60,7 @@ function MainPage() {
     </section>
   );
 }
+
+type ResultsType = AllPokemonData | SinglePokemonData | null;
 
 export default MainPage;
