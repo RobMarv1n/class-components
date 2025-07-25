3146fb723b00dfbf12146be2a38b4c18ffb3f010
Rob Marvin <fenalfan@gmail.com>
2025-07-25 19:51:13 +0300

    feat: add 2 separate methods to search for multiple Pokemon and 1 Pokemon

--- src/shared/api/service/pokemon.service.ts
+++ src/shared/api/service/pokemon.service.ts
@@ -2,15 +2,24 @@ import { START_SEARCH_ENDPOINT } from '../endpoints';
 import type { AllPokemonData } from '../types/AllPokemonTypes';
 import type { SinglePokemonData } from '../types/SinglePokemonTypes';
 
-export async function searchPokemon(
+export async function getPokemonList(limit: number, offset: number) {
+  const response = await fetch(
+    `${START_SEARCH_ENDPOINT}?limit=${limit}&offset=${offset}`
+  );
+  if (!response.ok) {
+    throw new Error('Failed to fetch Pokémon list');
+  }
+  return response.json();
+}
+
+export async function getSinglePokemon(
   query: string
 ): Promise<AllPokemonData | SinglePokemonData> {
-  const response = await fetch(`${START_SEARCH_ENDPOINT}${query}`);
-
+  const response = await fetch(
+    `${START_SEARCH_ENDPOINT}/${query.toLowerCase()}`
+  );
   if (!response.ok) {
-    throw new Error(`${response.status} ${response.statusText}`);
+    throw new Error('Pokémon not found');
   }
-
-  const data = await response.json();
-  return data;
+  return response.json();
 }
