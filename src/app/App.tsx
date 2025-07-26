8cc29713d5d373dbf4bf6ac4a8ea9ec6ae545a07
Rob Marvin <fenalfan@gmail.com>
2025-07-26 23:16:02 +0300

    chore: update routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import MainPage from '../pages/main/MainPage';
 import ErrorBoundary from './providers/ErrorBoundary/ErrorBoundary';
+import DetailedCharacterLayout from '../pages/main/ui/DetailedCharacterLayout';
 
 function App() {
   return (
     <>
       <ErrorBoundary>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          {/* <Route path="/about" element={<AboutPage />} /> */}
-          {/* <Route path="*" element={<NotFoundPage />} /> */}
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />}>
+              {/* <Route path="page" element={<AllCharactersPage />} /> */}
+              <Route
+                path="character/:id"
+                element={<DetailedCharacterLayout />}
+              />
+            </Route>
+            {/* <Route path="/about" element={<AboutPage />} /> */}
+            {/* <Route path="*" element={<NotFoundPage />} /> */}
+          </Routes>
+        </BrowserRouter>
       </ErrorBoundary>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App.tsx';
 import './app/styles/main.css';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = document.querySelector('#root');
 if (root) {
   createRoot(root).render(
     <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </StrictMode>
   );
 }
