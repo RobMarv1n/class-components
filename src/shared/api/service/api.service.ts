838379ae17907d97bc69be266365ea817a44f891
Rob Marvin <fenalfan@gmail.com>
2025-07-26 15:43:10 +0300

    fix: fix type for getSingleCharacter

--- src/shared/api/service/api.service.ts
+++ src/shared/api/service/api.service.ts
@@ -1,5 +1,5 @@
 import { START_SEARCH_ENDPOINT } from '../endpoints';
-import type { AllCharactersData } from '../types/types';
+import type { AllCharactersData, SingleCharacterData } from '../types/types';
 
 export async function getAllCharacters(
   name: string,
@@ -14,7 +14,9 @@ export async function getAllCharacters(
   return response.json();
 }
 
-export async function getSingleCharacter(id: number): Promise<CharacterData> {
+export async function getSingleCharacter(
+  id: number
+): Promise<SingleCharacterData> {
   const response = await fetch(`${START_SEARCH_ENDPOINT}/${id}`);
   if (!response.ok) {
     throw new Error('Character not found');
