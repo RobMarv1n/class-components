708736004822a8186fa244e4e4d3a8fd33ddd266
Rob Marvin <fenalfan@gmail.com>
2025-07-27 00:32:41 +0300

    chore: update routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import MainPage from '../pages/main/MainPage';
 import ErrorBoundary from './providers/ErrorBoundary/ErrorBoundary';
 import DetailedCharacterLayout from '../pages/main/ui/DetailedCharacterLayout';
+import NotFoundPage from '../pages/NotFound/NotFoundPage';
+import AboutPage from '../pages/About/AboutPage';
 
 function App() {
   return (
@@ -10,14 +12,13 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<MainPage />}>
-              {/* <Route path="page" element={<AllCharactersPage />} /> */}
               <Route
                 path="character/:id"
                 element={<DetailedCharacterLayout />}
               />
             </Route>
-            {/* <Route path="/about" element={<AboutPage />} /> */}
-            {/* <Route path="*" element={<NotFoundPage />} /> */}
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </BrowserRouter>
       </ErrorBoundary>
