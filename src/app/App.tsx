97f728d2b3d15e151763a13d6ef1fd6bf33428df
Rob Marvin <fenalfan@gmail.com>
2025-07-27 12:39:51 +0300

    chore: change main.tsx and App.tsx structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import MainPage from '../pages/main/MainPage';
-import ErrorBoundary from './providers/ErrorBoundary/ErrorBoundary';
 import DetailedCharacterLayout from '../pages/main/ui/DetailedCharacterLayout';
 import NotFoundPage from '../pages/NotFound/NotFoundPage';
 import AboutPage from '../pages/About/AboutPage';
@@ -8,20 +7,15 @@ import AboutPage from '../pages/About/AboutPage';
 function App() {
   return (
     <>
-      <ErrorBoundary>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<MainPage />}>
-              <Route
-                path="character/:id"
-                element={<DetailedCharacterLayout />}
-              />
-            </Route>
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </BrowserRouter>
-      </ErrorBoundary>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />}>
+            <Route path="character/:id" element={<DetailedCharacterLayout />} />
+          </Route>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
