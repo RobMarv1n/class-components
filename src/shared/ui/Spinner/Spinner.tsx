46f2268fd9cee5b0e07bec21fadbae71b269bf8f
Rob Marvin <fenalfan@gmail.com>
2025-08-03 19:11:57 +0300

    feat: remove spinner text content, update styles

--- src/shared/ui/Spinner/Spinner.tsx
+++ src/shared/ui/Spinner/Spinner.tsx
@@ -5,8 +5,7 @@ class Spinner extends Component {
   render() {
     return (
       <div className="spinner-container" role="status" aria-label="Loading">
-        <div className="spinner" />
-        <p>Loading...</p>
+        <div className="spinner"></div>
       </div>
     );
   }
