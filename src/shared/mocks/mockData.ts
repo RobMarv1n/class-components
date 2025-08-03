84651883c1f3252aaaf22bf10059c54bdc072804
Rob Marvin <fenalfan@gmail.com>
2025-08-04 01:53:09 +0300

    refactor: move data mock to a separate file

---
+++ src/shared/mocks/mockData.ts
@@ -0,0 +1,26 @@
+import type { AllCharactersData } from '../api/types/types';
+
+export const mockAllCharactersData: AllCharactersData = {
+  info: {
+    count: 1,
+    pages: 1,
+    next: null,
+    prev: null,
+  },
+  results: [
+    {
+      id: 1,
+      name: 'Rick Sanchez',
+      status: 'Alive',
+      species: 'Human',
+      gender: 'Male',
+      type: '',
+      origin: { name: 'Earth', url: '' },
+      location: { name: 'Earth', url: '' },
+      image: '',
+      episode: [],
+      url: '',
+      created: '',
+    },
+  ],
+};
