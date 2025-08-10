6176bfb5d8851282ba5ac6fad78e3dbacc87374a
Rob Marvin <fenalfan@gmail.com>
2025-08-11 02:25:55 +0300

    feat: update styles for about page

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -2,22 +2,28 @@ import { Link } from 'react-router-dom';
 
 export default function AboutPage() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        padding: '2rem',
-        gap: '1rem',
-      }}
-    >
-      <h1>About This App</h1>
-      <p>
+    <div className="flex flex-col p-8 gap-4 justify-center items-center">
+      <h1 className="text-2xl font-bold">About This App</h1>
+      <p className="">
         This is a demo Single Page Application displaying characters (e.g. from
         the Rick and Morty API).
       </p>
-      <Link to="/">Go back to homepage</Link>
-      <Link to={'https://rs.school'} target="_blank" rel="noopener noreferrer">
-        <img src="/logo-rs.svg" alt="RS School Logo" width={120} height={62} />
+
+      <Link to="/" className=" hover:underline transition-colors duration-200">
+        Go back to homepage
+      </Link>
+
+      <Link
+        to="https://rs.school"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-block"
+      >
+        <img
+          src="/logo-rs.svg"
+          alt="RS School Logo"
+          className="w-[120px] h-[62px]"
+        />
       </Link>
     </div>
   );
