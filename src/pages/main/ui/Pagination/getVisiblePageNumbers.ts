3fd8801ccd2f5c0dd79efc59f1bf56bf98294c3f
Rob Marvin <fenalfan@gmail.com>
2025-08-11 01:10:51 +0300

    refactor: convert magic numbers to constants, add logic to display less than 7 pages

--- src/pages/main/ui/Pagination/getVisiblePageNumbers.ts
+++ src/pages/main/ui/Pagination/getVisiblePageNumbers.ts
@@ -1,14 +1,23 @@
 import { isNumber } from '../../../../shared/utils/typeguards';
 
-export function getVisiblePageNumbers(currentPage: number, totalPages: number) {
-  const PAGES_AROUND = 2;
+const FULL_LIST_PAGE_LIMIT = 7;
+const PAGE_WINDOW_SIZE = 5;
+const PAGES_AROUND = 2;
+const LEFT_ELLIPSIS_THRESHOLD = 2;
 
-  const pageNumbers: (number | undefined)[] = Array.from({ length: 5 })
+export function getVisiblePageNumbers(currentPage: number, totalPages: number) {
+  if (totalPages <= FULL_LIST_PAGE_LIMIT) {
+    return Array.from({ length: totalPages }, (_, index) => index + 1);
+  }
+
+  const pageNumbers: (number | undefined)[] = Array.from({
+    length: PAGE_WINDOW_SIZE,
+  })
     .map((_, index) => index + currentPage - PAGES_AROUND)
     .filter((element) => element > 1 && element < totalPages);
 
   const firstVisiblePage = pageNumbers.at(0);
-  if (isNumber(firstVisiblePage) && firstVisiblePage > 2)
+  if (isNumber(firstVisiblePage) && firstVisiblePage > LEFT_ELLIPSIS_THRESHOLD)
     pageNumbers.unshift(undefined);
   const lastVisiblePage = pageNumbers.at(-1);
   if (isNumber(lastVisiblePage) && lastVisiblePage < totalPages - 1)
