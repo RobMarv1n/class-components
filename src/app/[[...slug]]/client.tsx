3f12dca2f2dfd4c14f3e2c8545478876bedfebb0
Rob Marvin <fenalfan@gmail.com>
2025-08-14 10:58:18 +0300

    feat: add ClientOnly component for migration app to next js

---
+++ src/app/[[...slug]]/client.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
+import dynamic from 'next/dynamic';
+import { StrictMode } from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../../store/store';
+import { ThemeProvider } from '../providers/ThemeProvider/ThemeProvider';
+import '../globals.css';
+
+const App = dynamic(() => import('../App'), { ssr: false });
+
+export function ClientOnly() {
+  return (
+    <StrictMode>
+      <ErrorBoundary errorComponent={() => null}>
+        <Provider store={store}>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </Provider>
+      </ErrorBoundary>
+    </StrictMode>
+  );
+}
