daa104318134a09c82c1155e88baee3b08292f0d
Rob Marvin <fenalfan@gmail.com>
2025-08-14 12:08:40 +0300

    chore(config): update vitest config with next js

--- vitest.config.js
+++ vitest.config.js
@@ -1,34 +1,32 @@
-import { mergeConfig } from 'vite';
 import { defineConfig } from 'vitest/config';
-import viteConfig from './vite.config';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './setup.js',
-      coverage: {
-        checkCoverage: true,
-        include: ['src/**/*.{js,jsx,ts,tsx}'],
-        exclude: [
-          'src/**/*.test.{js,jsx,ts,tsx}',
-          'src/**/*.spec.{js,jsx,ts,tsx}',
-          'src/index.{js,jsx,ts,tsx}',
-          'src/setupTests.{js,ts}',
-          'src/**/*.d.ts',
-          'src/**/mocks/*.{js,jsx,ts,tsx}',
-          'src/**/types/*.{js,jsx,ts,tsx}',
-          'src/main.tsx',
-        ],
-        thresholds: {
-          statements: 80,
-          branches: 50,
-          functions: 50,
-          lines: 50,
-        },
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setup.js',
+    coverage: {
+      checkCoverage: true,
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+        'src/**/mocks/*.{js,jsx,ts,tsx}',
+        'src/**/types/*.{js,jsx,ts,tsx}',
+        'src/main.tsx',
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
       },
     },
-  })
-);
+  },
+});
