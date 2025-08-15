2b7fafade4c25bba08fd86a0d2676f824f1b677c
Rob Marvin <fenalfan@gmail.com>
2025-08-15 05:24:44 +0300

    chore(config): add eslint ignore for node modules and next

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules', '.next'] },
   {
     extends: [
       js.configs.recommended,
