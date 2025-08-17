a9cca9d7325984c3b00a40895125cbca0c994226
Rob Marvin <fenalfan@gmail.com>
2025-08-17 05:57:20 +0300

    feat: add main page with next js

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useSearchParams, useRouter } from 'next/navigation';
+import { useCallback } from 'react';
+import { useCharactersSearch } from '../../pages/main/hooks/useCharactersSearch';
+import AllCharactersTable from '../../pages/main/ui/AllCharactersTable';
+import Pagination from '../../pages/main/ui/Pagination/Pagination';
+import SearchBox from '../../pages/main/ui/SearchBox/SearchBox';
+import SelectionToolbar from '../../pages/main/ui/SelectionToolbar/SelectionToolbar';
+import Header from '../../shared/ui/Header/Header';
+import DetailedCharacterLayout from './@details/character/[id]/page';
+
+export default function MainPage() {
+  const {
+    result,
+    error,
+    isLoading,
+    lastQuery,
+    currentPage,
+    totalPages,
+    handleSearch,
+  } = useCharactersSearch();
+
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handlePageChange = useCallback(
+    (page: number) => {
+      const params = new URLSearchParams(searchParams?.toString());
+      params.set('page', page.toString());
+      router.push(`/?${params.toString()}`);
+    },
+    [router, searchParams]
+  );
+
+  return (
+    <section className="main-page">
+      <Header />
+      <SearchBox onSearch={handleSearch} initialQuery={lastQuery} />
+      <div className="flex">
+        <AllCharactersTable data={result} error={error} isLoading={isLoading} />
+        <DetailedCharacterLayout />
+      </div>
+      {totalPages > 1 && (
+        <Pagination
+          currentPageNumber={currentPage}
+          totalPageCount={totalPages}
+          onPageChange={handlePageChange}
+        />
+      )}
+      <SelectionToolbar />
+    </section>
+  );
+}
