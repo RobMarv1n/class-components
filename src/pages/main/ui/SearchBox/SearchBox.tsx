a1c216c25b6305ae1ce216f521880c8270879be3
Rob Marvin <fenalfan@gmail.com>
2025-08-17 22:13:16 +0300

    feat: add a query update when changing props, update styles, remove module.css

--- src/pages/main/ui/SearchBox/SearchBox.tsx
+++ src/pages/main/ui/SearchBox/SearchBox.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BASE_API_PATH } from '../../../../shared/api/endpoints';
-import styles from './SearchBox.module.css';
 import Button from '../../../../shared/ui/Button/Button';
 import Input from '../../../../shared/ui/Input/Input';
 
 function SearchBox(props: SearchBoxProps) {
   const { onSearch, placeholder, initialQuery } = props;
   const [query, setQuery] = useState(initialQuery ?? '');
+
+  useEffect(() => {
+    if (initialQuery !== undefined) {
+      setQuery(initialQuery);
+    }
+  }, [initialQuery]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
@@ -18,15 +23,17 @@ function SearchBox(props: SearchBoxProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className={styles.searchBoxContainer}>
+    <form onSubmit={handleSubmit} className="w-full max-w-4xl flex-none">
+      <div className="flex justify-center items-center gap-4">
         <p>{BASE_API_PATH}</p>
         <Input
           onChange={handleChange}
           value={query}
           placeholder={placeholder || 'Enter character name'}
         />
-        <Button type="submit">Search</Button>
+        <Button type="submit" className="min-w-[100px]">
+          Search
+        </Button>
       </div>
     </form>
   );
