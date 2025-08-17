4a7c3c489a07ff3e4a334b26224b13831d6a471f
Rob Marvin <fenalfan@gmail.com>
2025-08-17 12:48:43 +0300

    feat: change AllCharactersTable with next/navigation, update styles

--- src/pages/main/ui/AllCharactersTable.tsx
+++ src/pages/main/ui/AllCharactersTable.tsx
@@ -3,9 +3,6 @@ import type {
   SingleCharacterData,
   AllCharactersData,
 } from '../../../shared/api/types/types';
-import { useNavigate, useLocation } from 'react-router-dom';
-import Spinner from '../../../shared/ui/Spinner/Spinner';
-import Input from '../../../shared/ui/Input/Input';
 import { toggleSelection } from '../../../store/slices/selectionSlice';
 import {
   useAppDispatch,
@@ -13,18 +10,28 @@ import {
 } from '../../../shared/hooks/reduxHooks';
 import { BASE_API_PATH } from '../../../shared/api/endpoints';
 import DataUploadError from '../../../shared/ui/DataUploadError/DataUploadError';
+import { useSearchParams, useRouter } from 'next/navigation';
+import Spinner from '../../../shared/ui/Spinner/Spinner';
+import Input from '../../../shared/ui/Input/Input';
 
 function AllCharactersTable({
   data,
   error,
   isLoading,
 }: AllCharactersTableProps) {
-  const navigate = useNavigate();
-  const location = useLocation();
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const dispatch = useAppDispatch();
   const selected = useAppSelector((state) => state.selection.selectedItems);
 
   const isSelected = (id: number) => selected.some((item) => item.id === id);
+
+  if (isLoading)
+    return (
+      <div className="flex-1 min-h-[calc(100vh-170px)]">
+        <Spinner />
+      </div>
+    );
 
   if (error) {
     return <DataUploadError error={error} />;
@@ -34,17 +41,14 @@ function AllCharactersTable({
     return <p>Nothing was found</p>;
   }
 
-  if (isLoading && !data) {
-    return <Spinner />;
-  }
-
   return (
-    <table style={{ position: 'relative', flexShrink: 0 }}>
+    <table className="relative m-3 flex-1 min-h-[600px] table-fixed">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Status</th>
-          <th>Species</th>
+          <th className="w-[200px]">Name</th>
+          <th className="w-[100px]">Status</th>
+          <th className="w-[180px]">Species</th>
+          <th className="w-[50px]">Select</th>
         </tr>
       </thead>
       <tbody>
@@ -52,17 +56,19 @@ function AllCharactersTable({
           <tr
             key={character.id}
             onClick={() => {
-              navigate(`/character/${character.id}/${location.search}`);
+              const params = new URLSearchParams(searchParams?.toString());
+              params.set('details', String(character.id));
+              router.push(`/?${params.toString()}`);
             }}
-            style={{ cursor: 'pointer' }}
+            className="h-8 cursor-pointer hover:bg-slate-200 hover:dark:bg-slate-600 wrap"
           >
             <td>{character.name}</td>
             <td>{character.status}</td>
             <td>{character.species}</td>
-            <td>
+            <td className="align-bottom">
               <Input
                 type="checkbox"
-                style={{ cursor: 'pointer', width: 20, height: 20, margin: 0 }}
+                className="cursor-pointer w-5 h-5 m-0"
                 checked={isSelected(character.id)}
                 onChange={() =>
                   dispatch(
