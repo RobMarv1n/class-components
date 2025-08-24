bb25e5fd2ef4810058f2fb68ae57d4b2e16f2157
Rob Marvin <fenalfan@gmail.com>
2025-08-24 20:25:41 +0300

    feat: add form input component

---
+++ src/shared/ui/inputs/FormInput.tsx
@@ -0,0 +1,105 @@
+import { type ChangeEvent, type InputHTMLAttributes, useId } from 'react';
+import type {
+  FieldValues,
+  Path,
+  UseFormRegister,
+  RegisterOptions,
+  FieldErrors,
+} from 'react-hook-form';
+import { getErrorMessage } from '../../utils/getErrorMessage';
+import type { Option } from '../../../store/store';
+
+function FormInput<T extends FieldValues>({
+  label,
+  name,
+  type = 'text',
+  register,
+  rules,
+  errors,
+  onChange,
+  options,
+  ref,
+  ...rest
+}: FormInputProps<T>) {
+  const errorMessage = errors?.[name]
+    ? getErrorMessage(errors[name])
+    : undefined;
+
+  let inputProps: InputHTMLAttributes<HTMLInputElement>;
+  if (register) {
+    inputProps = { ...register(name, rules), ...rest };
+
+    if (type === 'file' || type === 'checkbox' || type === 'radio') {
+      inputProps.onChange = onChange;
+    }
+  } else {
+    inputProps = { ...rest, onChange };
+  }
+
+  const isCheckboxOrRadio = type === 'checkbox' || type === 'radio';
+  const id = useId();
+  const listId = `${id}-list`;
+
+  return (
+    <div className="flex flex-col gap-1">
+      {!isCheckboxOrRadio && label && (
+        <label
+          htmlFor={String(name)}
+          className="text-sm font-medium text-gray-700"
+        >
+          {label}
+        </label>
+      )}
+
+      <div
+        className={`flex items-center gap-2 ${isCheckboxOrRadio ? 'mt-1' : ''}`}
+      >
+        <input
+          id={String(name)}
+          type={type}
+          ref={ref}
+          list={options ? listId : undefined}
+          {...inputProps}
+          className={`${
+            isCheckboxOrRadio
+              ? 'w-4 h-4 p-0 border-gray-300 rounded-sm'
+              : 'w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
+          } ${errorMessage ? 'border-red-500' : ''}`}
+        />
+
+        {isCheckboxOrRadio && label && (
+          <span className="text-sm text-gray-700">{label}</span>
+        )}
+      </div>
+
+      {options && (
+        <datalist id={listId}>
+          {options.map((opt) =>
+            typeof opt === 'string' ? (
+              <option key={opt} value={opt} />
+            ) : (
+              <option key={opt.value} value={opt.value}>
+                {opt.label}
+              </option>
+            )
+          )}
+        </datalist>
+      )}
+
+      {errorMessage && <p className="text-red-500 text-sm">{errorMessage}</p>}
+    </div>
+  );
+}
+
+type FormInputProps<T extends FieldValues> = {
+  label?: string;
+  name: Path<T>;
+  register?: UseFormRegister<T>;
+  rules?: RegisterOptions<T, Path<T>>;
+  errors?: FieldErrors<T> | Record<string, string>;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  options?: Option[];
+  ref?: React.Ref<HTMLInputElement>;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export default FormInput;
