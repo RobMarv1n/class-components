2ba220a5df8f795f4f686d9760aa6024a9ea0956
Rob Marvin <fenalfan@gmail.com>
2025-08-30 20:13:24 +0300

    chore: remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './main.css';
 import App from './app/App';
 
 const root = document.querySelector('#root');
 if (root) {
-  createRoot(root).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
+  createRoot(root).render(<App />);
 }
